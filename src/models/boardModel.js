042b2b202ed141af016bb9b6d0b8138baf7eab95
Huy0811 <duongchihuy.study@gmail.com>
2024-05-09 20:17:16 +0700

    validation model

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,5 @@
 import Joi from "joi"
+import { ObjectId } from "mongodb"
 import { GET_DATABASE } from "~/config/mongodb"
 
 const BOARD_COLLECTION_NAME = "boards"
@@ -12,9 +13,14 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
 const createNew = async (data) => {
   try {
-    const createdBoard = await GET_DATABASE().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    const validData = await validateBeforeCreate(data)
+    const createdBoard = await GET_DATABASE().collection(BOARD_COLLECTION_NAME).insertOne(validData)
 
     return createdBoard
   } catch (error) {
@@ -24,7 +30,9 @@ const createNew = async (data) => {
 
 const findOneById = async (id) => {
   try {
-    const result = await GET_DATABASE().collection(BOARD_COLLECTION_NAME).findOne({ _id: id })
+    const result = await GET_DATABASE()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOne({ _id: new ObjectId(id) })
 
     return result
   } catch (error) {
