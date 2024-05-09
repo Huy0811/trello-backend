8637f9cccac2f411c83a17f77f1d7cb263dfee72
Huy0811 <duongchihuy.study@gmail.com>
2024-05-09 14:56:00 +0700

    mode dev prod

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 import { StatusCodes } from "http-status-codes"
-// import { env } from '~/config/environment'
+import { env } from "~/config/environment"
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
@@ -12,7 +12,7 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
     stack: err.stack
   }
 
-  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+  if (env.BUILD_MODE !== "dev") delete responseError.stack
 
   res.status(responseError.statusCode).json(responseError)
 }
