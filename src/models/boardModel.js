a0e18216965861720a9d219b8688895a69f3fe13
Huy0811 <duongchihuy.study@gmail.com>
2024-05-11 10:39:41 +0700

    dnd columns

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -16,6 +16,7 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   updatedAt: Joi.date().timestamp("javascript").default(null),
   _destroy: Joi.boolean().default(false)
 })
+const INVALID_UPDATE_FIELDS = ["_id", "createdAt"]
 
 const validateBeforeCreate = async (data) => {
   return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
@@ -85,7 +86,24 @@ const pushColumnOrderIds = async (column) => {
         { returnDocument: "after" }
       )
 
-    return result.value
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const update = async (boardId, updateData) => {
+  try {
+    Object.keys(updateData).forEach((fieldName) => {
+      if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+        delete updateData[fieldName]
+      }
+    })
+    const result = await GET_DATABASE()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOneAndUpdate({ _id: new ObjectId(boardId) }, { $set: updateData }, { returnDocument: "after" })
+
+    return result
   } catch (error) {
     throw new Error(error)
   }
@@ -97,5 +115,6 @@ export const boardModel = {
   createNew,
   findOneById,
   getDetails,
-  pushColumnOrderIds
+  pushColumnOrderIds,
+  update
 }
--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -6,6 +6,6 @@ const Router = express.Router()
 
 Router.route("/").post(boardValidation.createNew, boardController.createNew)
 
-Router.route("/:id").get(boardController.getDetails).put()
+Router.route("/:id").get(boardController.getDetails).put(boardValidation.update, boardController.update)
 
 export const boardRoute = Router
